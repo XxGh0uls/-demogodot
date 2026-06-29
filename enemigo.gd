0032c6e3c93eae6c03363f9cd67e8805cecdf6d6
XxGh0uls <franco18gonza@gmail.com>
2026-06-28 22:12:19 -0500

    Add files via upload

---
+++ enemigo.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@onready var imagen_mori = $"../TextureRect"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+
+	print("¡El jugador tocó un enemigo!")
+	print("Nombre del cuerpo: ", body.name)
+	
+	imagen_mori.visible = true
---
+++ jugador.gd
@@ -0,0 +1,12 @@
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+extends CharacterBody2D
+var velocidad: float = 2200.0
+
+func _ready() -> void:
+# Mover al jugador al centro de la ventana
+	position = get_viewport_rect().size / 2
+
+func _physics_process(delta: float) -> void:
+	var direccion = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direccion * velocidad
+	move_and_slide()
